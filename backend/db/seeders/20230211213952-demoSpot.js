4de07093cf3c5acb608118e5fe569cd7d3a102b8
Alex Caballero <alexis.caballero@mavs.uta.edu>
2023-02-11 16:11:55 -0600

    added Spots models/seeder/route

---
+++ backend/db/seeders/20230211213952-demoSpot.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert("Spots" ,[{
+      address: "1542 Sedona Vortex",
+      city: "Sedona",
+      country: "United States",
+      lat: 36.36,
+      lng: 68.72,
+      name: "Sedona Vortex",
+      description: "a place with good views",
+      price: 135,
+      ownerId: 1,
+    }]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete("Spots" ,[{
+      address: "1542 Sedona Vortex",
+      city: "Sedona",
+      country: "United States",
+      lat: 36.36,
+      lng: 68.72,
+      name: "Sedona Vortex",
+      description: "a place with good views",
+      price: 135,
+      ownerId: 1,
+    }]);
+  }
+};
