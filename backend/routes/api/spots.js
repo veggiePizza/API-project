ef88ee3184ac2e899ab570c11eb355a27b1fbbdb
Alex Caballero <alexis.caballero@mavs.uta.edu>
2023-02-11 16:34:55 -0600

    created Reviews models/seed and an endpoint

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,11 +1,16 @@
 const express = require('express');
-const { Spot } = require('../../db/models');
+const { Spot, Review } = require('../../db/models');
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
     const spots = await Spot.findAll();
     if(spots) return res.status(200).json(spots);
+  });
+
+router.get('/:id/reviews', async (req, res) => {
+    const reviews = await Review.findAll({where: {spotId:req.params.id}});
+    if(reviews) return res.status(200).json(reviews);
   });
 
 module.exports = router;
