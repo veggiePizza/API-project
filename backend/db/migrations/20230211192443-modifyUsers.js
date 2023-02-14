8f62840090aa16b24c551a7124697e407167238d
Alex Caballero <alexis.caballero@mavs.uta.edu>
2023-02-14 15:46:50 -0600

    deploy up

--- backend/db/migrations/20230211192443-modifyUsers.js
+++ backend/db/migrations/20230211192443-modifyUsers.js
@@ -1,11 +1,11 @@
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
-
+/*
 let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
-}
+}*/
 
 module.exports = {
   async up (queryInterface, Sequelize) {
@@ -13,17 +13,20 @@ module.exports = {
 
       await queryInterface.addColumn("Users","firstName", {
         type: Sequelize.STRING,
-      },options),
+      }),//,options
       await queryInterface.addColumn("Users","lastName", {
         type: Sequelize.STRING,
-      },options),
+      }),//,options
 
     ]);
   },
 
   async down (queryInterface, Sequelize) {
-    options.tableName = "Users";
-    await queryInterface.removeColumn(options,"firstName");
-    await queryInterface.removeColumn(options,"lastName");
+    //options.tableName = "Users";
+    //await queryInterface.removeColumn(options,"firstName");
+    //await queryInterface.removeColumn(options,"lastName");
+
+    await queryInterface.removeColumn("Users","firstName");
+    await queryInterface.removeColumn("Users","firstName");
   }
 };
--- backend/db/seeders/20230213213248-demoReviewImage.js
+++ backend/db/seeders/20230213213248-demoReviewImage.js
@@ -9,6 +9,7 @@ if (process.env.NODE_ENV === 'production') {
 
 module.exports = {
   async up (queryInterface, Sequelize) {
+    options.tableName = 'ReviewImages';
     return queryInterface.bulkInsert("ReviewImages", [{
       url: "../pic.png",
       reviewId:1
