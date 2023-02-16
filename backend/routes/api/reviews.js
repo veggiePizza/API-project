2ae7a5e64bfb9f483150f495fcaeb2224f447603
Alex Caballero <alexis.caballero@mavs.uta.edu>
2023-02-15 19:51:07 -0600

    associations and a lot of endpoints

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -1,11 +1,63 @@
 const express = require('express');
-const { Reviews, ReviewImage } = require('../../db/models');
+const { Spot, Review, Booking, SpotImage, User, ReviewImage } = require('../../db/models');
+var Sequelize = require("sequelize");
+const review = require('../../db/models/review');
 
 const router = express.Router();
 
-router.get('/:id/images', async (req, res) => {
-    const images = await ReviewImage.findAll();
-    if(images) return res.status(200).json(images);
+//Get all Reviews of the Current User//add an .this?//fix previewImage
+router.get('/current', async (req, res) => {
+  const reviews = await Review.findAll({
+    where: { id: 1 },//heree
+    include: [
+      { model: User, attributes: ['id', 'firstName', 'lastName'] },
+      {
+        model: Spot,
+        attributes: {include: [],exclude: ['description','createdAt','updatedAt'] },
+        include: [{ model: SpotImage, attributes: ['url'] }]
+      },
+      { model: ReviewImage, attributes: ['id', 'url'] }
+    ],
   });
+  //********************** reviews is never null
+  if (reviews) return res.status(200).json(reviews);
+  else return res.status(404).json({ message: "Reviews couldn't be found" })
+
+});
+
+//Add an Image to a Review based on the Review's id
+router.post('/:id/images', async (req, res) => {
+  const { url } = req.body;
+  const review = await Review.findAll({ where: { id: req.params.id } });
+  if (review) {
+    const newImage = await ReviewImage.create({url})
+    if (newImage) {
+      return res.status(200).json(newImage);
+    }
+    //validation error check
+    return res.status(404).json({ message: "Image was not created" });
+  }
+  else {
+    return res.status(404).json({ message: "Review couldn't be found" });
+  }
+});
+
+//Edit a Review
+router.put('/:id', async (req, res) => {
+  const { review, stars } = req.body;
+  const newReview = await Review.findAll({ where: { id: req.params.id } });
+  if (newReview) {
+    await newReview.update({review,stars})
+    return res.status(200).json(newReview);
+  }
+  else 
+    return res.status(404).json({ message: "Review couldn't be found" });
+});
+
+router.get('/:id/images', async (req, res) => {
+  const images = await ReviewImage.findAll();
+  if (images) return res.status(200).json(images);
+});
+
 
 module.exports = router;
